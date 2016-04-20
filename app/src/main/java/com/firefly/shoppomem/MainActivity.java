d5dab2c342f30a3d20a78aff0350de29f3688523
Firelfy <maja.filakovic@gmail.com>
2016-04-20 16:00:04 +0200

    Added main screen and app bar on it.

--- app/src/main/java/com/firefly/shoppomem/MainActivity.java
+++ app/src/main/java/com/firefly/shoppomem/MainActivity.java
@@ -2,12 +2,41 @@ package com.firefly.shoppomem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mainToolbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainToolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(mainToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu. This adds items to action bar, if it's present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Handle action bar item clicks here. The action bar will
+        //automatically handle clicks on the Home/Up button, so long
+        //as you specify a parent activity in AndroidManifest.xml
+        int id = item.getItemId();
+
+        if(id == R.id.action_about) {
+            Toast.makeText(this, "About is clicked", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
